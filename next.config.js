eb4238e38ea5966ae3bf44a856229775887171f9
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2023-02-08 23:20:31 -0800

    changes to packages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: isProd ? '/LocalNewsTV.github.io/' : '',
+  images: {
+    unoptimized: true,
+  },
   images: {
     domains: ["cdn.weatherapi.com"]
   },
