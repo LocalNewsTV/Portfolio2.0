f8a8e9cbc82362e4c718b72815ed331f8dbd1277
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2023-02-09 21:08:18 -0800

    current State

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -2,12 +2,12 @@ import Image from "next/image";
 export const Hero = () => {
   return (
     <div className={"heroCont"}>
-      <Image 
+      {/* <Image 
         src={'/HeroLogo.png'}
         width={350}
         height={225}
         alt={"Matthew Logan Logo"}
-      />
+      /> */}
       <h1>Page Under Construction</h1>
     </div>
   )
