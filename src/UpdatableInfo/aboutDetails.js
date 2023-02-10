f851db61bda1ed1ce5b0090ead749fb9c4125a77
Matthew Logan <loganmatthewr@gmail.com>
2023-02-10 12:13:28 -0800

    Added content

--- src/UpdatableInfo/aboutDetails.js
+++ src/UpdatableInfo/aboutDetails.js
@@ -8,9 +8,11 @@ const details = {
     caption: "Hello"
   },
   writeup: {
-    beginnings: "beginnings",
-    gettingStarted: "getting started",
-    hobbies: "hobbies"
+    beginnings: "<p>Hello! I'm Matthew Logan, before attending Camosun College for the Information and Computer Systems program, I was a red seal chef for over 8 years, working in Victoria BC's hospitality sector. </p>",
+    gettingStarted: "<p>I Enrolled in Camosun College in 2021 to diverge from my previous career path after the Covid-19 Pandemic. From my time at Camosun College I've grown a wide range of skillsets</p>",
+    hobbies: "hobbies",
+    favouriteTools: "ES6 JavaScript, React, Python, MongoDB",
+    education: "",
   }
 }
 
